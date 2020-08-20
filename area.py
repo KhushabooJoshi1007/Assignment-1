9f60af8d5f6ffd70eac4b65ddfedde9e6db1e61d
KhushabooJoshi1007 <69675011+KhushabooJoshi1007@users.noreply.github.com>
2020-08-20 17:20:36 +0530

    Update area.py

--- area.py
+++ area.py
@@ -1,2 +1,2 @@
 #program to find out area of circle radius unput trough user
-print("area of cirlcle is:",3.14*int(input("enter circle radius"))**2)
+print("area of cirlcle is:",3.14*float(input("enter circle radius"))**2)
